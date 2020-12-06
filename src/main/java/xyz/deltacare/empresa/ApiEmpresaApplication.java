9a3e9a27c7c0a38926d8b4edb48f7f1564de65f3
David Carvalho <jdavidsilv@gmail.com>
2020-12-06 19:11:43 -0300

    primeiros arquivos adicionados

---
+++ src/main/java/xyz/deltacare/empresa/ApiEmpresaApplication.java
@@ -0,0 +1,13 @@
+package xyz.deltacare.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpresaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEmpresaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/deltacare/empresa/ApiEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.deltacare.empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmpresaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
