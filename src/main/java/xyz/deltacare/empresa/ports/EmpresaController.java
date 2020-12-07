d4dd68324a9eb210591ae870408e23dcaebc7cac
David Carvalho <jdavidsilv@gmail.com>
2020-12-07 07:40:53 -0300

    adição teste controller com 201

--- src/main/java/xyz/deltacare/empresa/ports/EmpresaController.java
+++ src/main/java/xyz/deltacare/empresa/ports/EmpresaController.java
@@ -1,7 +1,9 @@
 package xyz.deltacare.empresa.ports;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.deltacare.empresa.ports.dto.EmpresaDTO;
 
@@ -10,6 +12,7 @@ import xyz.deltacare.empresa.ports.dto.EmpresaDTO;
 public class EmpresaController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EmpresaDTO criar() {
         return null;
     }
